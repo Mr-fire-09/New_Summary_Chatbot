754ad8ceb321cd62dad46320b3212f20afa40f19
Neeraj Verma <neerajsingh009424@gmail.com>
2025-05-29 10:59:45 +0530

    update  the some chnages

---
+++ main.py
@@ -0,0 +1,6 @@
+from langchain_google_genai import ChatGoogleGenerativeAI  # Typo fixed: 'Genrative' → 'Generative'
+import os
+import requests
+from bs4 import BeautifulSoup  # Typo fixed: 'from bs4 import BeautifulSoup'
+from langchain.prompts import PromptTemplate  # Typo fixed: 'promts' → 'prompts'
+from langchain.chains import LLMChain
