80f22633da77fe6cb2fe8d2a775b6ae2f8615c77
Neeraj Verma <neerajsingh009424@gmail.com>
2025-05-29 11:10:43 +0530

    new file

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-from langchain_google_genai import ChatGoogleGenerativeAI  # Typo fixed: 'Genrative' → 'Generative'
-import os
+from langchain_google_genai import ChatGoogleGenrativeAI
+import os 
 import requests
-from bs4 import BeautifulSoup  # Typo fixed: 'from bs4 import BeautifulSoup'
-from langchain.prompts import PromptTemplate  # Typo fixed: 'promts' → 'prompts'
+from bs4  import BeautifulSoup
+from langchain.prompts import PromptTemplate  # ✅ Correct
+
 from langchain.chains import LLMChain
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+llm = ChatGoogleGenrativeAI(model="gemini-1.5-flash",temperature=0.7)
+
+
+def extract 
